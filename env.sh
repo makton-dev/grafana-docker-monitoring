27e70768f4e39dc1d41fce265debca5c3e858191
Mike Schneider <mschneider@se-llc.biz>
2026-02-09 09:28:49 -0700

    fix dependency

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PROM_ENDPOINT=[IP or Hostname of Prometheus server]
+export LOKI_ENDPOINT=[IP or Hostname of Loki server]
+export NODE_NAME=[Name of Node]
+export TENANT_ID=[Grafana/Loki Tenant ID]
